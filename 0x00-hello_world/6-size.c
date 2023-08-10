6b89bc8028cb62360dd4e3c3fcd8814ce0040c35
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-10 12:50:15 -0700

    6.size task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,17 @@
 /**
  * main - Entry point
  *
- * Description: A program that prints the size of various types on the computer using c language
+ * Description: A c  program that prints the size of various type
  *
  * Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
 	printf("Size of a char: %u byte(s)\n", sizeof(char));
 	printf("Size of an int: %u byte(s)\n", sizeof(int));
 	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %u byte(s)\n", sizeof(float));
+	printf("Size of a float: %u byte(s)", sizeof(float));
 	return (0);
 }
