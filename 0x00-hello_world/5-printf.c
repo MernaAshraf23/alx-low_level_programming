9368539fec3730308197eee29b1de2fc3fd9b89f
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-10 12:19:28 -0700

    5.Hello, printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that print with printf function using c language
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("ith proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
