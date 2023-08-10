77c3bc5f4e69ddd521e35cc77885999333dc2579
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-10 11:16:37 -0700

    4.Hello, puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main: entry point
+ *
+ * description: A program that print with puts function using c language
+ *
+ * return: Always 0 (Success)
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
