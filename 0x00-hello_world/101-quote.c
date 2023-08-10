42a68983415595ab90cbd52b51e1d19e895a8d1e
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-10 13:24:55 -0700

    8.error task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,10 @@
 /**
  * main - Entry point
  *
- * Description: A C program that print a line with standard error 
+ * Description: A C program that print a line with standard error
  *
- * Return: this time is 1 
+ * Return: this time is 1
 */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
