79df134e91492288e165163160662ea74015b5af
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-15 16:29:03 -0700

    10.add a&b task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - function that add two integer numbers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: result of sum a & b
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
