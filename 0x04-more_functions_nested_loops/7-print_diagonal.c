1d49cff270e4c44d67bd6746642665832af65848
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-17 15:14:00 -0700

    7.print diagonal test

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,8 +17,12 @@ void print_diagonal(int n)
 		for (line = 1; line <= n; line++)
 		{
 			for (space = 1; space <= line; space++)
-				_putchar(' ');
-			_putchar(92);
+			{
+				if (line == space)
+					_putchar(92);
+				else
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
