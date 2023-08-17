7147285122fa9887c1511144ab1e512b48e5c25b
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-17 15:25:50 -0700

    8.print square task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#inculde "main.h"
+
+/**
+ * print_square - function that print a square
+ *
+ * @size: size of the square
+*/
+
+void print_square(int size)
+{
+	int row, column;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (column = 1; column <= size; column++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
