14ca770901d3bd2726ae191bd2ae21cb3f3e1a20
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-17 13:12:12 -0700

    3.print numbers task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,9 +10,12 @@ void print_numbers(void)
 {
 	int num = 0;
 
-	do {
+	while (num <= 9)
+	{
 		_putchar(num + 48);
 		num++;
-	} while (num >= 0 && num <= 9);
+	}
 	_putchar('\n');
+
+	return (0);
 }
