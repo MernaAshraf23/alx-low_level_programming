91cef58aed06f656de66ae0927375d19bdb6b1af
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-17 18:11:10 -0700

    100.largest prime factor task

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -24,33 +24,6 @@ double _sqrt(double n)
 }
 
 /**
- * largest_prime_factor - function to find the largest prime factor of num
- *
- * @num: input for function
-*/
-
-void largest_prime_factor(long int num)
-{
-	int prmNum, largest;
-
-	while (num % 2 == 0)
-		num = num / 2;
-
-	for (prmNum = 3; prmNum <= _sqrt(num); prmNum += 2)
-	{
-		while (num % prmNum == 0)
-		{
-			num = num / prmNum;
-			largest = prmNum;
-		}
-	}
-
-	if (num > 2)
-		largest = num;
-	printf("%d\n", largest);
-}
-
-/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -58,6 +31,14 @@ void largest_prime_factor(long int num)
 
 int main(void)
 {
-	largest_prime_factor(612852475143);
+	long i, largest;
+	long num = 612852475143;
+
+	for (i = 1; i <= _sqrt(num); i++)
+	{
+		if (num % i == 0)
+			largest = num / i;
+	}
+	printf("%d\n", largest);
 	return (0);
 }
