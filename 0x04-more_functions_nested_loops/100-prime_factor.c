c1be0b31017d61c0ff2b5504cbab3ac100eca19c
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-17 18:19:35 -0700

    100.task

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,29 +1,6 @@
 #include <stdio.h>
 
 /**
- * _sqrt - print the square root
- *
- * @n: input for function
- *
- * Return: return square root of n
-*/
-
-double _sqrt(double n)
-{
-	float sqrt, tmp;
-
-	sqrt = n / 2;
-	tmp = 0;
-
-	while (sqrt != rmp)
-	{
-		tmp = sqrt;
-		sqrt = (n / tmp + tmp) / 2;
-	}
-	return (sqrt);
-}
-
-/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -34,10 +11,13 @@ int main(void)
 	long i, largest;
 	long num = 612852475143;
 
-	for (i = 1; i <= _sqrt(num); i++)
+	for (i = 1; i <= num; i++)
 	{
 		if (num % i == 0)
+		{
 			largest = num / i;
+			num = largest;
+		}
 	}
 	printf("%d\n", largest);
 	return (0);
