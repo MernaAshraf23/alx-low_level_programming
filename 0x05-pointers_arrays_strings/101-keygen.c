ba4366e688d04dbc3cb55ea35aba25b939f5ac86
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-21 10:19:50 -0700

    101.generat password task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: generate random password
+ *
+ * Return : Always 0 (Success)
+*/
+
+int main(void)
+{
+	int sum;
+	char ch;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		ch = rand() % 128;
+		sum += ch;
+		_putchar(ch);
+	}
+	_putchar(2772 - sum);
+	return (0);
+}
