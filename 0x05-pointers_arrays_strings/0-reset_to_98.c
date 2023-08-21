87e56fddf23f48c9643f280f508f0bca1a128482
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-21 05:37:19 -0700

    0.reset to 98 task & first push for dir

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function takes a pointer to an int as parameter
+ *                 and update value to 98
+ *
+ * @n: int parameter for function
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
