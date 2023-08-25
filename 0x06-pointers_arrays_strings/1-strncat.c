d1286ff5a63638b7b2dca880bd414bb24b35fd76
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-25 14:36:01 -0700

    1.concatenating at most n bytes task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates two strings,
+ *                      it will use at most n bytes
+ *
+ * @dest: first string parameter
+ * @src: second string parameter
+ * @n: number of byres
+ *
+ * Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int c1, c2;
+
+	c1 = 0;
+	while (dest[c1] != '\0')
+		c1++;
+
+	c2 = 0;
+	while (c2 < n && src[c2] != '\0')
+	{
+		dest[c1] = src[c2];
+		dest++;
+		src++;
+	}
+
+	dest[c1] = '\0';
+	return (dest);
+}
