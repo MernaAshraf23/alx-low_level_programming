9dedbf7444be95e550c4bc11e058a2c644fc7495
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-25 17:43:17 -0700

    100.encoding string by rot13 task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encode a string using rot13
+ *
+ * @str : string parameter for function
+ *
+ * Return: string
+*/
+
+char *rot13(char *str)
+{
+	int i, j;
+	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char dataRot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == data[i])
+			{
+				str[i] = dataRot[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
