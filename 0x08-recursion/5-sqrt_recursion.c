2030e725a4fae495a41df71c338a2b08b6bcb53d
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-30 01:40:14 -0700

    5.square recursion task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * sqrt - function retuen square
+ * @n: input parameter
+ * @x: input parameter value
+ * Return: int
+*/
+
+int sqrt(int n, int x)
+{
+	if (x * x == n)
+		return (x);
+	else if (x * x < n)
+		return (sqrt(n, x + 1));
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - function that returns the natural
+ *                      square root of num without loops
+ * @n: int parameter
+ *
+ * Return: int value
+*/
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n, 1));
+}
