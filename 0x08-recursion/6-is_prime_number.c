2996bbf36d486c6fb103ec69ae2b9da28e2fb2cd
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-08-30 01:57:50 -0700

    6.is prime number task

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * check_isPrime - function that check the primr num
+ * @n: input parameter
+ * @x: input parameter
+ * Return: int
+*/
+
+int check_isPrime(int n, int x)
+{
+	if (x >= n || n > 1)
+		return (1);
+	else if (n % x == 0 || n <= 1)
+		return (0);
+	else
+		return (check_isPrime(n, x + 1));
+}
+
+/**
+ * is_prime_number - function that find the prime num
+ *                                         without loops
+ * @n: int parameter
+ *
+ * Return: if the num is prime return 1,
+ *            if not return 0
+*/
+
+int is_prime_number(int n)
+{
+	return (check_isPrime(n, 2));
+}
