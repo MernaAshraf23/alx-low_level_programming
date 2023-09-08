be470be3c37a7c4e959bf1fc83fc7c3cd47325a2
MernaAshraf23 <mernaashraf6023@gmail.com>
2023-09-08 11:35:05 -0700

    2.calloc task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -36,12 +36,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	memo = malloc(sizeof(int) * nmemb);
+	memo = malloc(size * nmemb);
 
 	if (memo == NULL)
 		return (NULL);
 
-	_memset(memo, 0, sizeof(int) * nmemb);
+	_memset(memo, 0, size * nmemb);
 
 	return (memo);
 }
